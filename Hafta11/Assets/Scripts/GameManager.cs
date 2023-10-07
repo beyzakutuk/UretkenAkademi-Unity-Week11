605e975be1bdae5fd9c7be5bbfe003196c946ab4
Beyza Kütük <beyzakutuk55@gmail.com>
2023-10-07 19:29:30 +0300

    Update GameManager.cs
    Player Id assingned

--- Hafta11/Assets/Scripts/GameManager.cs
+++ Hafta11/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Linq;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
@@ -41,6 +42,16 @@ public class GameManager : MonoBehaviourPunCallbacks
         PlayerController playerScript = playerobj.GetComponent<PlayerController>();
 
         playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    public PlayerController GetPlayer(int playerid)
+    {
+        return players.First(x => x.id == playerid);
+    }
+
+    public PlayerController GetPlayer(GameObject playerobj)
+    {
+        return players.First(x => x.gameObject == playerobj);
     }
 
     // Update is called once per frame
